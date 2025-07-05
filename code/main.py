87b5f6ab503ad298c96c998a03df38f1a02bc9c2
jvorvick <vorvickjesse@gmail.com>
2025-07-04 18:54:39 -0700

    init commit

---
+++ code/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+# general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    display_surface.fill('blue')
+    pygame.display.set_caption('Space Shooter')
+    pygame.display.update()
+
+pygame.quit()
