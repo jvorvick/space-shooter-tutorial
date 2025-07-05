37e253d368b0e3fb2aa237d832b6716f2ca8d027
jvorvick <vorvickjesse@gmail.com>
2025-07-05 16:11:47 -0700

    vectors

--- code/main.py
+++ code/main.py
@@ -5,20 +5,25 @@ from random import randint
 
 # general setup
 pygame.init()
+# flags = pygame.FULLSCREEN
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
 surf.fill('orange')
 x = 100
 
-# importing an image
+# imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = 1
+player_direction = pygame.math.Vector2(2,-1)
+# print(player_direction)
+# print(player_direction.reflect_ip(pygame.math.Vector2(1, 0)))
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -30,6 +35,7 @@ laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
+    dt = clock.tick(60) / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,9 +50,13 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
   
     # player movement
-    player_rect.x += player_direction * 0.4
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
+    
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction.reflect_ip(pygame.math.Vector2(1,0))
+    if player_rect.top <= 0 or player_rect.bottom >= WINDOW_HEIGHT:
+        player_direction.reflect_ip(pygame.math.Vector2(0,1))
+    player_rect.center += player_direction * player_speed * dt
+
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
