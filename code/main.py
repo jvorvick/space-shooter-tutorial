69fb171366f40c41cceb1d82db65a59f9bde83fa
jvorvick <vorvickjesse@gmail.com>
2025-07-06 10:51:20 -0700

    get input

--- code/main.py
+++ code/main.py
@@ -20,8 +20,8 @@ x = 100
 # imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1,1)
-player_speed = 1000
+player_direction = pygame.math.Vector2()
+player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -38,6 +38,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #     print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rect.center = event.pos
+
+    # input
+    # print(pygame.mouse.get_rel())
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
 
     # draw the game
     display_surface.fill('darkgray')
@@ -46,20 +58,6 @@ while running:
 
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-  
-    # player movement
-    # if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-    #     player_direction.reflect_ip(pygame.math.Vector2(1,0))
-    # if player_rect.top < 0 or player_rect.bottom > WINDOW_HEIGHT:
-    #     player_direction.reflect_ip(pygame.math.Vector2(0,1))
-    
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
-
-    player_rect.center += player_direction * player_speed * dt
-
     display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
