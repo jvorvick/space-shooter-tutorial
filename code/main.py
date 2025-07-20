c03fd75c510ef840714e9f344317e9c54f0d2fcb
jvorvick <49791235+jvorvick@users.noreply.github.com>
2025-07-20 14:32:22 -0700

    sound, game finish!

--- code/main.py
+++ code/main.py
@@ -88,6 +88,7 @@ class AnimatedExplosion(pygame.sprite.Sprite):
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
         self.rect = self.image.get_frect(center = pos)
+        explosion_sound.play()
 
     def update(self, dt):
         self.frame_index += 20 * dt
@@ -95,28 +96,6 @@ class AnimatedExplosion(pygame.sprite.Sprite):
             self.image = self.frames[int(self.frame_index) % len(self.frames)]
         else:
             self.kill()
-
-
-# class Explosion(pygame.sprite.Sprite):
-#     def __init__(self, surfs, pos, groups, anim_speed):
-#         super().__init__(groups)
-#         self.surfs = surfs
-#         self.surf_key = 0
-#         self.image = surfs[self.surf_key]
-#         self.rect = self.image.get_frect(center = pos)
-#         self.anim_frame_start = pygame.time.get_ticks()
-#         self.last_anim_frame = next(reversed(self.surfs.items()))[0]
-#         self.anim_speed = anim_speed
-
-#     def update(self, dt):
-#         self.surf_key = self.anim_speed * round(
-#             (pygame.time.get_ticks() - self.anim_frame_start) / self.anim_speed
-#         )
-#         if self.surf_key <= self.last_anim_frame:
-#             if self.surf_key in self.surfs:
-#                 self.image = self.surfs[self.surf_key]
-#         else:
-#             self.kill()
 
 def collisions():
     global running
@@ -130,9 +109,6 @@ def collisions():
         if collided_sprites:
             laser.kill()
             AnimatedExplosion(explosion_frames, laser.rect.midtop, all_sprites)
-
-
-            # Explosion(explosion_surfs, laser.rect.midtop, (all_sprites, explosion_sprites), explosion_speed)
 
     # pygame.sprite.groupcollide(laser_sprites, meteor_sprites, True, True)
 
@@ -160,25 +136,11 @@ font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'), 40)
 explosion_frames = [pygame.image.load(join('images', 'explosion', f'{i}.png')).convert_alpha() for i in range(21)]
 
 laser_sound = pygame.mixer.Sound(join('audio', 'laser.wav'))
-
-# explosion_speed = 10
-
-# explosion_paths = listdir(join('images', 'explosion'))
-# explosion_paths.sort(key=lambda f : int(splitext(f)[0]))
-# # explosion_paths.sort(key=lambda f : int(''.join(filter(str.isdigit, f))))
-
-# explosion_values = []
-# for p in explosion_paths:
-#     explosion_values.append(
-#         pygame.image.load(join('images', 'explosion', p)).convert_alpha()
-#     )
-
-# explosion_keys = []
-# for i, x in enumerate(explosion_values):
-#     explosion_keys.append(i * explosion_speed)
-
-# explosion_surfs = dict(zip(explosion_keys, explosion_values))
-
+laser_sound.set_volume(0.5)
+explosion_sound = pygame.mixer.Sound(join('audio', 'explosion.wav'))
+game_music = pygame.mixer.Sound(join('audio', 'game_music.wav'))
+game_music.set_volume(0.4)
+game_music.play(loops= -1)
 
 #sprites
 all_sprites = pygame.sprite.Group()
